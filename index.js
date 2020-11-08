915393acba112233d0d26ad3f7959bf9572efacb
Nabendu <nabendu.biswas@gmail.com>
2020-11-08 07:21:36 +0530

    Part-4 done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const redux = require('redux')
+const reduxLogger = require('redux-logger')
 const createStore = redux.createStore
 const combineReducers = redux.combineReducers
+const applyMiddleware = redux.applyMiddleware
+const logger = reduxLogger.createLogger()
 const BUY_EGG = 'BUY_EGG'
 const BUY_CHICKEN = 'BUY_CHICKEN'
 
@@ -69,9 +72,9 @@ const rootReducer = combineReducers({
   egg: eggReducer,
   chicken: chickenReducer
 })
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(logger))
 console.log('Initial State ', store.getState())
-const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()))
+const unsubscribe = store.subscribe(() => {})
 store.dispatch(buyEgg())
 store.dispatch(buyEgg())
 store.dispatch(buyChicken())
